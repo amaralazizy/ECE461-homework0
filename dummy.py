be627b2e0ad9c70512544a20a71d6a2f8966f6ef
zaradisht2003 <bola.yosry@gmail.com>
2024-08-29 20:13:01 +0300

    Corrected a syntax error in modulus function

--- dummy.py
+++ dummy.py
@@ -27,7 +27,7 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero!")
     return a / b
-def modulus(a,b)
+def modulus(a,b):
     return a%b
 
 if __name__ == "__main__":
